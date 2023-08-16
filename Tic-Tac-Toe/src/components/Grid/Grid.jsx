2d1ab086d7649e83682509d4b5701e1831f181e9
Lakshman kumar <lakshmankumar2603@gmail.com>
2023-08-16 12:49:29 +0530

    grid that contain card

--- Tic-Tac-Toe/src/components/Grid/Grid.jsx
+++ Tic-Tac-Toe/src/components/Grid/Grid.jsx
@@ -1,13 +1,46 @@
+import { useState } from "react";
 import Card from "../Card/Card";
 import './grid.css';
+import CheckWinner from "../helper/CheckWinner";
 
 const Grid = ({numberOfCards}) => {
+
+    const [turn, setTurn] = useState(true);
+    const [board, setBoard] = useState(Array(numberOfCards).fill(''));
+    const [winner, setWinner] = useState(null);
+
+    function play(index){
+
+        if(turn){
+            board[index] = 'O';
+        }
+        else{
+            board[index] = 'X';
+        }
+
+        const win = CheckWinner(board, turn ? 'O' : 'X');
+        if(win){
+            setWinner(win)
+        }
+        setBoard([...board]);
+        setTurn(!turn);
+    }
     return(
-        <div className="grid">
-            {Array(numberOfCards).fill(<Card />).map((el, Idx) => {
-                return <Card key={Idx} />
+        <>
+            {
+                winner && (
+                    <>
+                        <h1 className="turn-highlight">Winner is : {winner}</h1>   
+                    </>
+                )
+            }
+            <h1 className="turn-highlight">Current Turn : {(turn) ? 'O' : 'X'}</h1>
+            <div className="grid">
+            {board.map((value, Idx) => {
+                return <Card onPlay={play} player={value} key={Idx} index={Idx} />
             })}
         </div>
+        </>
     )
 }
 
