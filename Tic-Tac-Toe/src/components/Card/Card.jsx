e8b28693061c9e29fd88fda28e8da573bf44b3c4
Lakshman kumar <lakshmankumar2603@gmail.com>
2023-08-16 12:48:40 +0530

    All cards of game

--- Tic-Tac-Toe/src/components/Card/Card.jsx
+++ Tic-Tac-Toe/src/components/Card/Card.jsx
@@ -1,13 +1,22 @@
 import Icon from "../Icons/Icons";
-import './card.css'
-import '../../App.css'
+import "./card.css";
+import "../../App.css";
 
-const Card = ({iconName}) => {
-    return(
-        <div className="cards">
-            <Icon  name={iconName}/>
-        </div>
-    )
-}
+const Card = ({ onPlay, player, index }) => {
+
+    let icon = <Icon />
+    if(player == "X"){
+        icon = <Icon name={'cross'} />
+    }
+    else if(player == "O"){
+        icon = <Icon name={'circle'} />
+    }
+
+  return (
+    <div className="cards" onClick={() => onPlay(index)}>
+      {icon}
+    </div>
+  );
+};
 
 export default Card;
